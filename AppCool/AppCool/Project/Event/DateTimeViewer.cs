f54a55d2f44b4554e1fd108401c4b6f1cfbe8a21
Egor Leskov <egoe.leskov.03@mail.ru>
2021-12-01 14:47:43 +0300

    fix: date time viewer

--- AppCool/AppCool/Project/Event/DateTimeViewer.cs
+++ AppCool/AppCool/Project/Event/DateTimeViewer.cs
@@ -11,34 +11,40 @@ namespace AppCool.Project.Event
         { 
             get
             {
+                /*
                 if (FixedDateTime == null)
                     throw new Exception("Fixate has not been done.");
 
                 if (UnfixedDateTime == null)
                     throw new Exception("Unfixate has not been done.");
-
+                */
+                
                 return UnfixedDateTime - FixedDateTime;
             }
         }
 
         public void Fixate()
         {
+            /*
             if (FixedDateTime != null)
                 throw new Exception("Fixate has been done.");
 
             if (UnfixedDateTime != null)
                 throw new Exception("Unfixate has been done.");
+            */
 
             FixedDateTime = DateTime.Now;
         }
 
         public void Unfixate()
         {
+            /*
             if (FixedDateTime == null)
                 throw new Exception("Fixate has not been done.");
 
             if (UnfixedDateTime != null)
                 throw new Exception("Unfixate has been done.");
+            */
 
             UnfixedDateTime = DateTime.Now;
         }
