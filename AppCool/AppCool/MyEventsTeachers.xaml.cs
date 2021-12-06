dd6f769afecb8fb334dc7f3591de951419f4be7e
Egor Leskov <egoe.leskov.03@mail.ru>
2021-12-06 13:52:39 +0300

    Update: MyEventTeacher Design

---
+++ AppCool/AppCool/MyEventsTeachers.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppCool
+{
+    /// <summary>
+    /// Логика взаимодействия для MyEventsTeachers.xaml
+    /// </summary>
+    public partial class MyEventsTeachers : Page
+    {
+        public MyEventsTeachers()
+        {
+            InitializeComponent();
+        }
+    }
+}
